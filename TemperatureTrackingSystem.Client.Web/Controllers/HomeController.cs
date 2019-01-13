1b222ad6c1ce019b662bce250a524fbb8dc4d1a8
SaiKushalPolisetty <saikushal.p@gmail.com>
2019-01-13 12:42:45 +0530

    Contact page changes

--- TemperatureTrackingSystem.Client.Web/Controllers/HomeController.cs
+++ TemperatureTrackingSystem.Client.Web/Controllers/HomeController.cs
@@ -18,8 +18,6 @@ namespace TemperatureTrackingSystem.Client.Web.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
